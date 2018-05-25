b222a209a379736e6dde08be588d5697dedcd9d6
Tobias Timm <tbs.timm@gmail.com>
2018-05-26 00:55:30 +0200

    A bit more responsive layout for the gh-pages

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -34,43 +34,45 @@ const IndexPage = () => (
           </picture>
         </div>
         <div className="install-container">
-          <h2>
-            <span>Install</span>
-          </h2>
-          <p>Do you want to try Raijū?</p>
-          <div className="cli">
-            <label>Install via CLI</label>
-            <div className="code">
-              <pre>
-                <code>
-                  <span>$ </span>
-                  code --install-extension tobiastimm.raiju
-                </code>
-              </pre>
+          <div className="install-container-wrapper">
+            <h2>
+              <span>Install</span>
+            </h2>
+            <p>Do you want to try Raijū?</p>
+            <div className="cli">
+              <label>Install via CLI</label>
+              <div className="code">
+                <pre>
+                  <code>
+                    <span>$ </span>
+                    code --install-extension tobiastimm.raiju
+                  </code>
+                </pre>
+              </div>
             </div>
-          </div>
-          <div className="cli">
-            <div className="instructions-header">
-              <label>Install via</label>
-              <svg
-                className="vs-code"
-                width="21"
-                height="22"
-                viewBox="0 0 21 22"
+            <div className="cli">
+              <div className="instructions-header">
+                <label>Install via</label>
+                <svg
+                  className="vs-code"
+                  width="21"
+                  height="22"
+                  viewBox="0 0 21 22"
+                >
+                  <path
+                    fill="#6f7899"
+                    d="M21.9980902,7.05213528 L22,24.9558586 L17.0004653,27 L8.24542044,18.3059039 L2.9904497,22.3987722 L1,21.3771089 L1.00199979,10.6348745 L3.00181348,9.60581456 L8.26108889,13.7020894 L16.9946509,5 L21.9980902,7.05213528 Z M16.4966475,12.1654844 L11.4990677,15.9880111 L16.4986023,19.8405089 L16.4966475,12.1654844 Z M3.4986824,13.7064321 L3.50168388,18.3041032 L5.99842148,15.9897298 L3.4986824,13.7064321 Z"
+                    transform="translate(-1 -5)"
+                  />
+                </svg>
+              </div>
+              <a
+                className="download-btn"
+                href="https://marketplace.visualstudio.com/items?itemName=tobiastimm.raiju"
               >
-                <path
-                  fill="#6f7899"
-                  d="M21.9980902,7.05213528 L22,24.9558586 L17.0004653,27 L8.24542044,18.3059039 L2.9904497,22.3987722 L1,21.3771089 L1.00199979,10.6348745 L3.00181348,9.60581456 L8.26108889,13.7020894 L16.9946509,5 L21.9980902,7.05213528 Z M16.4966475,12.1654844 L11.4990677,15.9880111 L16.4986023,19.8405089 L16.4966475,12.1654844 Z M3.4986824,13.7064321 L3.50168388,18.3041032 L5.99842148,15.9897298 L3.4986824,13.7064321 Z"
-                  transform="translate(-1 -5)"
-                />
-              </svg>
+                Download
+              </a>
             </div>
-            <a
-              className="download-btn"
-              href="https://marketplace.visualstudio.com/items?itemName=tobiastimm.raiju"
-            >
-              Download
-            </a>
           </div>
         </div>
       </section>
